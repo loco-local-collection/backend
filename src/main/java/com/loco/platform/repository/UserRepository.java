1bd592ea19dc7f997b1927f2dbde343b161a777a
choi <nippert@naver.com>
2025-03-10 15:07:31 +0900

    chore: 테스트 코드 환경 구축

--- src/main/java/com/loco/platform/repository/UserRepository.java
+++ src/main/java/com/loco/platform/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.loco.platform.repository;
 
 import com.loco.platform.domain.Users;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
+
+    List<Users> findByNickname(String nickname);
 }
